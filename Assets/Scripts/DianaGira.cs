f8ddbc25d76a68cd2ea6c2bd4544ad6c64516cf3
Toño Gimeno <antoniogimenoo@hotmail.com>
2019-09-20 10:57:17 +0200

    Initial commit

---
+++ Assets/Scripts/DianaGira.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DianaGira : MonoBehaviour
+{
+    Animator animacionDeLaDiana;
+
+    //Creamos una variable tipo Animator para poder cargar mas adelante las animaciones
+    void Start()
+    {
+        //Guardamos el componente Animator en la variable de antes
+        animacionDeLaDiana = GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        //Al pulsar con el raton en la diana ejecutamos la animacion con el trigger
+        //Cuidado la mayusculas y minusculas
+        EjecutarAnimacion();
+        //Con Invoke retrasamos la funcion EjecutarAnimacion 2 segundos
+        Invoke("EjecutarAnimacion", Random.Range(0,6));
+    }
+
+    //Tenemos que crear una funcion para poder invocarla
+    void EjecutarAnimacion()
+    {
+        //Volvemos a llamar al trigger al cambiar la animacion
+        animacionDeLaDiana.SetTrigger("Activar");
+    }
+}
---
+++ Assets/Scripts/Patoclick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patoclick : MonoBehaviour
+{
+    //Creamos una ariable de tipo Animator para poder modificar la velocidad de la animacion
+    public Animator controlador;
+    [SerializeField]
+    int vida = 3;
+
+    //Usamos SerializeField para mostrar una variable privada en Unity
+    [SerializeField]
+    //La variable velocidad cambiara la velocidad de animacion
+    float velocidad = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // controlador = GetComponent<Animator>();
+        controlador.speed = velocidad;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Pinchado");
+
+        vida--;
+
+        if (vida == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

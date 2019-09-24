03cab9767137a1e5a4d28ce26c8a2fe90282e2d8
Toño <antoniogimenoo@hotmail.com>
2019-09-24 19:00:04 +0200

    CameraAnimation
    Animacion camara

---
+++ Assets/Scripts/ControlDisparo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Cuando hago click en la pantalla ejecuta una animacion de la camara de forma
+/// aleatoria
+/// </summary>
+
+public class ControlDisparo : MonoBehaviour
+{
+    public Animator animCamera;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("raton click");
+        }
+    }
+
+}
